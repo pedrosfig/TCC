3f82575e1d5a428d257142bfb125d411eeec07db
Bruno <brunocsouza1998@gmail.com>
2020-08-09 20:42:28 -0300

    refiz tudo q tinha feito

--- Séries_Artificiais.R
+++ Séries_Artificiais.R
@@ -2,5 +2,29 @@ library(forecast)
 library("openxlsx")
 library(smooth)
 require(graphics)
+require(latex)
 
-é nois
+ 
+#####Period#####
+mult_p = rep(NA,60)
+for (i in 1:60){mult_p[i] = cos(pi*i/6)}
+plot(mult_p,type='l')
+
+add_p = rep(NA,60)
+for (i in 1:60){add_p[i] = cos(pi*i/6)*2500}
+plot(mult_p,type='l')
+
+######Trend######
+trend = rep(NA,60)
+for (i in 1:60){trend[i]=400*i}
+plot(trend,type = 'l')
+
+windows()
+par(mfrow = c(1,1))
+plot(trend+add_p)
+plot(trend*mult_p)
+
+
+
+
+
