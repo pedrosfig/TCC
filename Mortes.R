398486593351ee8bca03ef20087691a331b3033b
Pedro Soares <pedrosfig@hotmail.com>
2020-10-12 11:42:58 -0300

    reuniao

--- Mortes.R
+++ Mortes.R
@@ -25,19 +25,24 @@ mortes_treino
 mortes_teste <- subset(mortes, start = 49)
 mortes_teste
 
-hw_mortes_treino <- HoltWinters(mortes_treino, seasonal = "add")  #a=0.78 b=0 c=0.43
+hw_mortes_treino <- HoltWinters(mortes_treino, seasonal = "add") # a=0.78 b=0 c=0.43
 hw_mortes_treino
 
 arima_mortes_treino <- auto.arima(mortes_treino)    # ARIMA(0,1,1)(1,1,0)[12]
 arima_mortes_treino
 
+
+# ACF e PACF
+acf(ts(mortes_treino), lag.max = 30)
+pacf(ts(mortes_treino), lag.max = 30)
+
 serie_diff=ts(diff(mortes_treino,lag = 1))
-acf(serie_diff,xlim = c(0,20))
-pacf(serie_diff,xlim = c(0,20))
+acf(serie_diff, lag.max = 30)
+pacf(serie_diff, lag.max = 30)
 
 serie_diff12 = diff(serie_diff, lag = 12)
-acf(serie_diff12,xlim = c(0,20))
-pacf(serie_diff12,xlim = c(0,20))
+acf(serie_diff12, lag.max = 30)
+pacf(serie_diff12, lag.max = 30)
 
 aj1 = arima(mortes_treino,c(0,1,0),seasonal = list(order=c(1,0,0),period = 12))
 aj2 = arima(mortes_treino,c(0,1,1),seasonal = list(order=c(1,0,0),period = 12))
@@ -57,13 +62,17 @@ cbind(AICs,BICs,SSEs)
 #podemos ver que o ajuste com menores erros é o 4 (auto.arima)
 
 #Para a primeira parte do Arima, podemos ver que o acf quebra 
-x[0]
-plot(mortes, ylim=c(6000, 12000), main = "Holt Winters: Mortes")
+
+
+
+# Teste infantil inicial
+{plot(mortes, ylim=c(6000, 12000), main = "Holt Winters: Mortes")
 lines(predict(hw_mortes_treino, 24), col="red", lty=2)
 lines(predict(arima_mortes_treino, 24)$pred,col = 'blue', lty=2)
 legend("top", inset=.05,
        c("Real","HW_auto", "SARIMA"), lwd=1, lty=c(1,2,2), col=c("black","red", "blue")) 
-#FYI: nesse caso o arima ganhou 
+}
+#FYI: nesse caso o arima ganhou (HW_auto é uma bosta)
 
 
 
@@ -115,9 +124,9 @@ SSE_arima_auto <- sum((predict(arima_mortes_treino, 24)$pred - mortes_teste)^2) 
 SSE_HW_auto <- sum((predict(hw_mortes_treino, 24) - mortes_teste)^2)             # HW automatico
 
 
-{plot(c(SSE_HW_CV, SSE_arima_auto, SSE_HW_auto), col=c("green","gray","red"), type = "p", main = "Test Error", ylab="SSE")
+{plot(c(SSE_HW_CV, SSE_arima_auto, SSE_HW_auto), col=c("green","dark orange","red"), type = "p", main = "Test Error", ylab="SSE")
 legend("top", inset=.05, c("HW Cross-Validation", "ARIMA automatico","HW automatico"), lty = 1, 
-       col=c("green","gray","red")) 
+       col=c("green","dark orange","red")) 
 }
 segments(x0=1, y0=SSE_HW_CV, x1=2, y1=SSE_arima_auto, lty=3)
 segments(x0=2, y0=SSE_arima_auto, x1=3, y1=SSE_HW_auto, lty=3)
